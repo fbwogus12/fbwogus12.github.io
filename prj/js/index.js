4764561293cb49b30e0bd4e309096bc86ee383d4
fbwogus12 <60141322+fbwogus12@users.noreply.github.com>
2021-08-16 02:22:59 +0900

    Update index.js

--- prj/js/index.js
+++ prj/js/index.js
@@ -1,1 +1,34 @@
 
+
+function setClock() {
+    var curDate = new Date();
+    var hour = curDate.getHours();
+    var marker = hour >= 12 ? 'PM' : 'AM';
+    var hour_ = hour % 12;
+    hour = (hour_ == 0) ? 12 : (hour_ < 10) ? '0' + hour_ : hour_;
+
+    var min = setTime(curDate.getMinutes());
+    var sec = setTime(curDate.getSeconds());
+    var year = curDate.getFullYear();
+    var month = curDate.getMonth();
+    var date = curDate.getDate();
+    
+    date = parseInt(date) < 10 ? '0' + date : date;
+    const w = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var dateForDay = new Date(year, month, date);
+    var day = w[dateForDay.getDay()];
+
+    const m = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct', 'Nov.', 'Dec'];
+    document.getElementsByClassName('time')[0].innerHTML = hour + ':' + min + ':' + sec + ', ' + marker;
+    document.getElementsByClassName('date')[0].innerHTML = day + ', '+m[month] + ' ' + date + ', ' + year;
+}
+
+function setTime(time) {
+    var result = parseInt(time) < 10 ? '0' + time : time;
+    return result;
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    setClock();
+    setInterval(setClock, 1000);
+});
