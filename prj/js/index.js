dbb0ff636303860d0f7cef7ee2a7345b8c82c694
fbwogus12 <60141322+fbwogus12@users.noreply.github.com>
2021-08-17 04:11:12 +0900

    Update index.js

--- prj/js/index.js
+++ prj/js/index.js
@@ -12,7 +12,7 @@ function setClock() {
     var year = curDate.getFullYear();
     var month = curDate.getMonth();
     var date = curDate.getDate();
-    
+
     date = parseInt(date) < 10 ? '0' + date : date;
     const w = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var dateForDay = new Date(year, month, date);
@@ -20,7 +20,7 @@ function setClock() {
 
     const m = ['Jan.', 'Feb.', 'Mar.', 'Apr.', 'May.', 'Jun.', 'Jul.', 'Aug.', 'Sept.', 'Oct', 'Nov.', 'Dec'];
     document.getElementsByClassName('time')[0].innerHTML = hour + ':' + min + ':' + sec + ', ' + marker;
-    document.getElementsByClassName('date')[0].innerHTML = day + ', '+m[month] + ' ' + date + ', ' + year;
+    document.getElementsByClassName('date')[0].innerHTML = day + ', ' + m[month] + ' ' + date + ', ' + year;
 }
 
 function setTime(time) {
@@ -29,6 +29,15 @@ function setTime(time) {
 }
 
 window.addEventListener('DOMContentLoaded', function () {
+    //대문 시계
     setClock();
     setInterval(setClock, 1000);
+
+    //엘리먼트
+    var rec = document.getElementsByClassName('rec')[0];
+
+    //이벤트
+    rec.addEventListener('click', function () {
+        location.href = 'recommendKeyboard.html';
+    })
 });
